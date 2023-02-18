f2390dbfc57b4c5c7dca4e2f6a5a59fd06f38856
Purrro <45691888+Purrro@users.noreply.github.com>
2023-02-18 18:39:12 +0200

    Server communication refactoring

--- src/services/gameServices.js
+++ src/services/gameServices.js
@@ -2,55 +2,58 @@ import axios from "axios";
 
 const serverAdress = "http://localhost:3001";
 
+// Get all games
 export const getGamesList = async () => {
   try {
-    const res = await axios.get(`${serverAdress}/showallgames`);
+    const res = await axios.get(`${serverAdress}/games`);
     return res.data;
   } catch (e) {
     console.log(e);
   }
 };
-
+// Get game by ID
 export const getGameDetails = async (gameId) => {
   try {
-    const res = await axios.get(`${serverAdress}/getgamedetails/${gameId}`);
+    const res = await axios.get(`${serverAdress}/games/${gameId}`);
     return res.data;
   } catch (e) {
     console.log(e);
   }
 };
-
-export const getQuestions = async (gameID) => {
-  try {
-    const res = await axios.get(`${serverAdress}/getquestions/${gameID}`);
-    return res.data;
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-export const getAnswers = async (questionId) => {
-  try {
-    const res = await axios.get(`${serverAdress}/getanswers/${questionId}`);
-    return res.data;
-  } catch (e) {
-    console.log(e);
-  }
-};
-
+// Create new game
 export const postAddGame = async (name) => {
   try {
-    const res = await axios.post(`${serverAdress}/creategame`, { name });
+    const res = await axios.post(`${serverAdress}/games/create`, { name });
     return res.data.insertId;
   } catch (e) {
     console.log(e);
   }
 };
+// Remove Game
+export const deleteGame = async (gameId) => {
+  try {
+    const res = await axios.post(`${serverAdress}/games/delete`, {
+      gameId,
+    });
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
+// Get questions by game ID
+export const getQuestions = async (gameID) => {
+  try {
+    const res = await axios.get(`${serverAdress}/questions/${gameID}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 // Create a new question with { body, game_id }
 export const postNewQuestion = async (body, game_id) => {
   try {
-    const res = await axios.post(`${serverAdress}/createquestion`, {
+    const res = await axios.post(`${serverAdress}/questions/create`, {
       body,
       game_id,
     });
@@ -59,13 +62,33 @@ export const postNewQuestion = async (body, game_id) => {
     console.log(e);
   }
 };
+// Remove Question
+export const removeQuestion = async (questionId) => {
+  try {
+    const res = await axios.post(`${serverAdress}/questions/delete`, {
+      questionId,
+    });
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
+// Get answers by question ID
+export const getAnswers = async (questionId) => {
+  try {
+    const res = await axios.get(`${serverAdress}/answers/${questionId}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 // Create new answer with { body, iscorrect, question_id }
 export const postAnswers = async (body, isCorrect, questionId) => {
   console.log("GameServices: " + body, isCorrect, questionId);
 
   try {
-    const res = await axios.post(`${serverAdress}/createanswers`, {
+    const res = await axios.post(`${serverAdress}/answers/create`, {
       body,
       isCorrect,
       questionId,
@@ -76,26 +99,6 @@ export const postAnswers = async (body, isCorrect, questionId) => {
   }
 };
 
-// Remove Question
-export const removeQuestion = async (questionId) => {
-  try {
-    const res = await axios.post(`${serverAdress}/deletequestion`, {
-      questionId,
-    });
-    return res.data;
-  } catch (e) {
-    console.log(e);
-  }
-};
 
-// Remove Game
-export const deleteGame = async (gameId) => {
-  try {
-    const res = await axios.post(`${serverAdress}/deletegame`, {
-      gameId,
-    });
-    return res.data;
-  } catch (e) {
-    console.log(e);
-  }
-};
+
+
